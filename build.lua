ffdc071db55e12a7a7cee3d6ede07592bed232d4
yublum <yuuybonito@gmail.com>
2025-07-30 01:44:45 -0300

    project setup

---
+++ build.lua
@@ -0,0 +1,5 @@
+return {
+  { "b", "./build.sh lin" },
+  { "w", "./build.sh win" },
+  { "e", "./generate_entities.py" },
+}
--- build.sh
+++ build.sh
@@ -1,3 +1,36 @@
-#!/usr/bin/env sh
+if [ "$#" -ne 1 ]; then
+  echo "usage: ./build win|lin"
+  exit 1
+fi
 
-x86_64-w64-mingw32-gcc ./main.c -o game
+OUT="game"
+FLAGS="-Wall -Wextra -Werror -Wpedantic -std=c23"
+DEF="-DDEV"
+SRC=$(find ./src/ -type f -name "*.c" | tr '\n' ' ')
+LIBS="\
+-L./vendor/ \
+"
+INCS="\
+-I./vendor/ \
+"
+
+if [ "$1" = "win" ]; then
+  DEF+=" -DWINDOWS"
+  LIBS+="\
+-lwin-glfw3 \
+-lgdi32 \
+"
+  OUT+=".exe"
+  CC=x86_64-w64-mingw32-gcc
+elif [ "$1" = "lin" ]; then
+  DEF+=" -DLINUX"
+  LIBS+="\
+-llin-glfw3 \
+-lm \
+"
+  CC=gcc
+else
+  echo "invalid target platform $1"
+fi
+
+$CC $FLAGS $DEF $SRC $LIBS $INCS -o $OUT
